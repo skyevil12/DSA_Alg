943ad3191cf24355b5573eceea638143c4a4e386
WINSTON.HUNG <WINSTON.HUNG@DELTAWW.COM>
2019-10-03 20:54:43 +0800

    Follow up part of C6 1.java

--- Winston/Ch6/1.java
+++ Winston/Ch6/1.java
@@ -11,7 +11,7 @@ class GFG {
 		int totalCnt = Integer.parseInt(scanner.nextLine().trim());
 		for(int i = 0; i < totalCnt; i++) {
 		    int dummy = Integer.parseInt(scanner.nextLine().trim());
-		    outputNonrepetitionChar(scanner.nextLine());
+		    generalOutputNonrepetitionChar_2(scanner.nextLine());
 		}
 	}
 	
@@ -33,5 +33,64 @@ class GFG {
 	    }
 	    
 	    System.out.println("" + -1);
+	}
+	
+	/**
+	*1
+	 4
+	 你我你我他他媽的咧
+	*/
+	private static void generalOutputNonrepetitionChar_2(String in) {
+	    System.out.println(in);
+		Map<Integer, Object> charCountMap = new HashMap<Integer, Object>();
+		Object onceObj = new Object();
+		Object twiceMoreObj = new Object();
+		Object obj = null;
+		
+		for(int i = 0; i < in.length();) {
+			int cp = in.codePointAt(i);
+			i += Character.charCount(cp);
+			
+			if(null == charCountMap.get(cp)) {
+				charCountMap.put(cp, onceObj);
+			} else {
+				charCountMap.put(cp, twiceMoreObj);
+			}
+		}
+		
+		for(int i = 0; i < in.length();) {
+			int cp = in.codePointAt(i);
+			i += Character.charCount(cp);
+			if(onceObj == charCountMap.get(cp)) {
+				System.out.println(new String(Character.toChars(cp)));
+				return;
+			}
+		}
+		
+		System.out.println("" + -1);
+	}
+	
+	private static void generalOutputNonrepetitionChar(String in) {
+		Map<Character, Integer> charCountMap = new HashMap();
+		char[] inCharArray = in.toCharArray();
+		
+		for(char ch : inCharArray) {
+			Character element = (Character)ch;
+			if(!charCountMap.containsKey(element)) {
+				charCountMap.put(element, 1);
+			} else {
+				charCountMap.put(element, charCountMap.get(element) + 1);
+			}
+		}
+		
+		for(char ch : inCharArray) {
+			Character element = (Character)ch;
+			if(1 == charCountMap.get(element)) {
+				System.out.println("" + element);
+				return;
+			}
+		}
+		
+		System.out.println("" + -1);
 	}
 }
